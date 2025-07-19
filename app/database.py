7b40b9c132c65ac99cc279dce58dd82f835e444b
Tony Blum <tonyblum@me.com>
2025-07-19 07:37:03 -0600

    v1

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://tokentapuser:secretpassword@localhost:5432/tokentapdb"
+load_dotenv()  # Load from .env
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if SQLALCHEMY_DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL not set in .env file")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
